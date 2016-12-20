1ddd60a3d02b58ae896641a5d6c34e1cb7f21c5a
fallblank <fallblank525@gmail.com>
2016-12-20 17:51:35 +0800

    songzhengteng:add shop page

---
+++ src/main/java/com/parknshop/controller/ShopController.java
@@ -0,0 +1,17 @@
+package com.parknshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by SONG on 2016/12/20.
+ */
+@Controller
+@RequestMapping("/shop")
+public class ShopController {
+    @RequestMapping(value = "detail",method = RequestMethod.GET)
+    public String showDetails(){
+        return "shop/shop.html";
+    }
+}
