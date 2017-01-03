c5e42421d78ccebe59ff68e0d921efd45a049030
qlong <870603569@qq.com>
2017-01-03 15:48:30 +0800

    realse 3

--- src/main/java/com/parknshop/service/baseImpl/IDefineString.java
+++ src/main/java/com/parknshop/service/baseImpl/IDefineString.java
@@ -7,5 +7,5 @@ public interface IDefineString {
     //用户 session 的key 值
     String SESSION_USER = "user";
     //用于定义 网页地址
-    String HOME_ID = "http://localhost:8080";
+    String HOME_ID = "http://www.miss-rabbit.cc";
 }
