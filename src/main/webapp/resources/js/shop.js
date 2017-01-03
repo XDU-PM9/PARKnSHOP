2dbe3c6b4dfe6710c84cfaa1a878af5331b09d45
weizhiyong <zhi_yong_wei@163.com>
2017-01-03 17:34:17 +0800

    购物车，总价格

--- src/main/webapp/resources/js/shop.js
+++ src/main/webapp/resources/js/shop.js
@@ -2,15 +2,26 @@
  * Created by wei on 16-12-31.
  */
 
+/**
+ * 全选
+ */
 function selectAll() {
     $(':checkbox').each(function () {
+        //如果没有被禁用
             if (!$(this).is(':disabled')) {
+                //选中
                 $(this).attr("checked","checked");
             }
         }
     )
 }
 
+/**
+ * 当数量改变时，发起ajax请求，同时判断是否超出库存，以决定是否禁用checkbox
+ * @param cartId
+ * @param amount
+ * @param goodsAmount
+ */
 function amountChange(cartId, amount, goodsAmount) {
     $.ajax({
         url: "/changeAmount",
@@ -22,18 +33,25 @@ function amountChange(cartId, amount, goodsAmount) {
         success: function (msg) {
             if ('success' == msg) {
                 if (amount > goodsAmount) {
+                    //取消选中
+                    document.getElementById(cartId).checked=false;
                     document.getElementById(cartId).setAttribute("disabled", "disabled");
                     document.getElementById(cartId).style.cursor = "not-allowed";
-                    document.getElementById(cartId).removeAttribute("checked");
+                    // document.getElementById(cartId).removeAttribute("checked");
                 } else {
                     document.getElementById(cartId).removeAttribute("disabled");
                     document.getElementById(cartId).style.cursor = "auto";
+                    //更改成功后改变总价
+                    $('#xiaoji'+cartId).html($('#danjia'+cartId).html()*amount);
                 }
             }
         }
     })
 }
 
+/**
+ * 提交购物车
+ */
 function submitCart() {
     var data = "";
     $(':checkbox').each(function () {
